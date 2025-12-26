70a37335729a6903616a4a6fa4a6a50eb9da5fc3
Suresh Kaleyannan <hello@suresh.app>
2025-12-26 14:02:46 +0800

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@
     return (
       <div className='hero'>
           <h1>
-              <span>ShareBoard</span> Online Clipboard
+              <span>CPBoard</span> Online Copy Paste Clipboard
           </h1>
           <p>enables you to seamlessly copy and paste your content across various Internet-connected devices. Save the Generated ID to access your content anytime without expiration.</p>
       </div>

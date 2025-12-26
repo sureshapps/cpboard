c2504ec6f9b3ccf28a5d67024281f20d3bd17e3a
Suresh Kaleyannan <hello@suresh.app>
2025-12-26 13:59:58 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
     <div className="navbar-container">
       <nav className="navbar">
         <h1>
-          <span>Share</span>Board
+          <span>CP</span>Board
         </h1>
         <a
           rel="noreferrer"
           target="_blank"
-          href="https://github.com/agamtyagi1717/shareboard"
+          href="https://suresh.app/"
         >
-          <button className="button-0">GitHub</button>
+          <button className="button-0">Developer</button>
         </a>
       </nav>
     </div>
